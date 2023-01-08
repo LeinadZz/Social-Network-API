c787e70df5e2008ad647a09ae8ef360f7bb4a197
Daniel <leinad6ix.z@gmail.com>
2023-01-08 01:06:27 -0800

    created controller to get a single thought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -12,6 +12,19 @@ const thoughtController = {
         res.status(500).json(err);
       });
   },
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          return res.status(404).json({ message: 'No thought with this id!' });
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
 };
 
 module.exports = thoughtController
