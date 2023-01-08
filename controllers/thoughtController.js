2a0870391b7550edfc2f9011c3541821a7dc096a
Daniel <leinad6ix.z@gmail.com>
2023-01-08 01:12:30 -0800

    created controller to update a thought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -46,6 +46,20 @@ const thoughtController = {
         res.status(500).json(err);
       });
   },
+  updateThought(req, res) {
+    Thought.findOneAndUpdate({ _id: req.params.thoughtId }, { $set: req.body }, { runValidators: true, new: true })
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          return res.status(404).json({ message: 'No thought with this id!' });
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
 };
 
 module.exports = thoughtController
