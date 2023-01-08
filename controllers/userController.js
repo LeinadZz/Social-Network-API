f8ae4242e2b574988a77fd8ffee9d447c22dcc1a
Daniel <leinad6ix.z@gmail.com>
2023-01-08 00:51:20 -0800

    created updateUser controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -41,5 +41,30 @@ const userController = {
         res.status(500).json(err);
       });
   },
+  // update a user
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { 
+        _id: req.params.userId 
+      },
+      {
+        $set: req.body,
+      },
+      {
+        runValidators: true,
+        new: true,
+      }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res.status(404).json({ message: 'No user with this id!' });
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
 
 }
