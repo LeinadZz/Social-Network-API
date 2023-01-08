c756916d4a1db148130c1811e04d41c707ee6edc
Daniel <leinad6ix.z@gmail.com>
2023-01-08 00:56:23 -0800

    created addFriend controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -81,5 +81,18 @@ const userController = {
         res.status(500).json(err);
       });
   },
+  addFriend(req, res) {
+    User.findOneAndUpdate({ _id: req.params.userId }, { $addToSet: { friends: req.params.friendId } }, { new: true })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res.status(404).json({ message: 'No user with this id!' });
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
 
 }
