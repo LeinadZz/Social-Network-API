d34b149eb17962db27b4bfe9f6708532e502cbb6
Daniel <leinad6ix.z@gmail.com>
2023-01-08 00:47:41 -0800

    created getSingleUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,4 +13,22 @@ const userController = {
         res.status(500).json(err);
       });
   },
+  // get single user
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select('-__v')
+      .populate('friends')
+      .populate('thoughts')
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res.status(404).json({ message: 'No user with this id!' });
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
 }
