16c4bfd03069efa5f1d4e2376c2054ef41fe29c4
Daniel <leinad6ix.z@gmail.com>
2023-01-08 00:54:57 -0800

    created deleteUser controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -66,5 +66,20 @@ const userController = {
         res.status(500).json(err);
       });
   },
+  deleteUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.userId })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res.status(404).json({ message: 'No user with this id!' });
+        }
+      })
+      .then(() => {
+        res.json({ message: 'User deleted!' });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
 
 }
