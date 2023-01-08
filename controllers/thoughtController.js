f4b5330d36537c1f02fd81356311909d5bd58044
Daniel <leinad6ix.z@gmail.com>
2023-01-08 01:08:02 -0800

    created controller to create a single thought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -25,6 +25,27 @@ const thoughtController = {
         res.status(500).json(err);
       });
   },
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then((dbThoughtData) => {
+        return User.findOneAndUpdate(
+          { _id: req.body.userId },
+          { $push: { thoughts: dbThoughtData._id } },
+          { new: true }
+        );
+      })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res.status(404).json({ message: 'Thought created but no user with this id!' });
+        }
+
+        res.json({ message: 'Thought successfully created!' });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
 };
 
 module.exports = thoughtController
